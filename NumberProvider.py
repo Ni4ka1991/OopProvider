33a89c2fa3010e6ff2008ebd51d8eb783e172543
Ni4ka1991 <astahova_nica@mail.ru>
2021-05-28 12:49:22 +0300

    Add file NumberProvider.py app.py

--- NumberProvider.py
+++ NumberProvider.py
@@ -1,30 +1,30 @@
 # NumberProvider.py
-
+from os import system
 from random import randint
 from time import sleep
 
 class NumberProvider:
     def __init__( self, min = -100, max = 100 ):
-        if( type(min) == int and type(max) == int):
-            self.min = min
-            self.max = max
-        else:
-            print( "Type error" )
+        self.min = min
+        self.max = max
 
     def generate( self ):
         return randint( self.min, self.max )
 
-    def whenPositive( self, positiveCB ):
-        self.positiveCB = positiveCB
+#    def whenPositive( self, positiveCB ):
+#        self.positiveCB = positiveCB
 
-    def whenNegative( self ):
-        #2. ???
-        pass
+#    def whenNegative( self, negativCB ):
+#        self.negativ = negativCB
+#        pass
 
     def start( self ):
+        system( "clear" )
         while True:
             number = self.generate()
-            if( number  >= 0 ):
-                self.positiveCB( number )
-            # 2. ???
+#            print( number )
+#            if( number  >= 0 ):
+#                self.positiveCB( number )
+#            else:
+#                self.negativCB( number )
             sleep( 1 ) 
--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
+from os import system
 from NumberProvider import NumberProvider
 
 def positiveAction( number ):
     print( "Got an positive number >>> ", number )
 
-provider = NumberProvider()
-provider.whenPositive( positiveAction )
+def negativeAction( number ):
+    print( "Got an negativ number >>>", number )
+
+def action( number ):
+    print( "Got a number >>> ", number )
+
+provider = NumberProvider( -10, 15  )
+#provider.whenPositive( positiveAction )
+#provider.whenNegative( negativeAction )
 provider.start()
+#print( number )
 
